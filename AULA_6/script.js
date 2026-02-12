7832c08ad96de2e1e5aaa8ed49fd3dbb786937c0
bielvictor1001-collab <bielvictor1001@gmail.com>
2026-02-11 21:55:22 -0300

    Add files via upload

---
+++ AULA_6/script.js
@@ -0,0 +1,43 @@
+console.log('4 TIPOS DE DADOS BASICOS PRIMITIVOS')
+// CONTROL + F5
+// STRINGS CARACTERE TEXTO 
+console.log("isso é' um texto 1")
+console.log("isso é um texto 2")
+console.log("10000") 
+
+
+// NULO
+console.log(null)
+
+
+// INDEFINIDO
+console.log(undefined)
+
+
+
+//DADOS BOLEANOS
+// resposta a uma condição / dado nasceu em uma das duas condições 
+console.log('saida', 10>2)
+console.log(true)
+console.log(false)
+
+
+// NUMEROS INTEIROS - INTEGER
+// idade - ano -  quantidade
+
+
+console.log(20)
+console.log(10)
+console.log(-1)
+console.log(0)
+
+
+// REAIS FLOATS
+// dinheiro -  altura - peso 
+console.log(10.9)
+console.log(5.9)
+console.log(25.88)
+
+
+
+
